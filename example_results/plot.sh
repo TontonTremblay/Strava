175fcd65cd3655f1f3ce195bf209645d70bee277
TontonTremblay <tonton.tremblay@gmail.com>
2015-08-20 12:23:25 -0400

    added an example results

---
+++ example_results/plot.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#This is an example to produce the graph 
+#from the data in .data file
+#make sure the path is correct in order to find 
+#the right font. 
+python ../plotDistribution.py "camillien_houde_JTremblay.data"
+
